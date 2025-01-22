b46166d76ea4bbead426a6c9037cf8852d2b99a3
VinzentErtl <vinzent.ertl@student.htldornbirn.at>
2025-01-22 12:01:56 +0100

    Rest again renewed

--- src/vinz/java/Rest/index.js
+++ src/vinz/java/Rest/index.js
@@ -5,7 +5,6 @@ const PORT = 3000;
 
 app.use(express.json());
 
-// Sample data
 let people = [
     { id: 1, name: 'Alice', age: 25 },
     { id: 2, name: 'Bob', age: 30 },
@@ -30,21 +29,22 @@ app.get('/people/:id', (req, res) => {
 // POST
 app.post('/people', (req, res) => {
     const newPerson = {
-        id: people.length ? people[people.length - 1].id + 1 : 1, 
+        id: people.length + 1,
         name: req.body.name,
         age: req.body.age
     };
     people.push(newPerson);
-    res.status(201).json(newPerson);
+    res.json(newPerson);
 });
 
 // PUT
 app.put('/people/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const personIndex = people.findIndex(p => p.id === id);
-    if (personIndex !== -1) {
-        people[personIndex] = { ...people[personIndex], ...req.body };
-        res.json(people[personIndex]);
+    const id = parseInt(req.params.id);
+    const person = people.find(p => p.id === id);
+    if (person) {
+        person.name = req.body.name || person.name;
+        person.age = req.body.age || person.age;
+        res.json(person);
     } else {
         res.status(404).json({ error: 'Person not found' });
     }
@@ -52,10 +52,10 @@ app.put('/people/:id', (req, res) => {
 
 // DELETE
 app.delete('/people/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const personIndex = people.findIndex(p => p.id === id);
-    if (personIndex !== -1) {
-        const deletedPerson = people.splice(personIndex, 1);
+    const id = parseInt(req.params.id);
+    const index = people.findIndex(p => p.id === id);
+    if (index !== -1) {
+        const deletedPerson = people.splice(index, 1);
         res.json(deletedPerson);
     } else {
         res.status(404).json({ error: 'Person not found' });
