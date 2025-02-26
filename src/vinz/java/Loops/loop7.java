1396828926cb1476de41172c5f137febacc11c6d
VinzentErtl <vinzent.ertl@student.htldornbirn.at>
2025-02-26 13:09:13 +0100

    loops almost finished

---
+++ src/vinz/java/Loops/loop7.java
@@ -0,0 +1,9 @@
+public class loop7 {
+    public static void main(String[] args) {
+        int product = 1;
+        for(int i=1; i<=5; i++){
+        product *=i;
+        
+    }
+    System.out.println(product);}
+}
